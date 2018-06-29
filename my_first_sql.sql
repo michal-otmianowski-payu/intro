42b2f0708774a90035fb432c205e689e83e43cf2
Michał Otmianowski <Michal.Otmianowski@payu.com>
2018-06-29 14:27:42 +0200

    grouping and aggregation added

--- my_first_sql.sql
+++ my_first_sql.sql
@@ -1,10 +1,13 @@
 SELECT 
     column1
     ,column2
+    ,SUM(column3)
 FROM
     my_table
 where
     column1>0
     and column2='ala ma kota'
     and column3 is not NULL
-
+group by
+    column1,
+    column2
