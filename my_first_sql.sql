bf5ab30dd08b01f5ca43b296fb41b5ff815da049
Michał Otmianowski <Michal.Otmianowski@payu.com>
2018-06-29 14:40:29 +0200

    changes merged

--- my_first_sql.sql
+++ my_first_sql.sql
@@ -3,7 +3,11 @@ SELECT
     ,column2
     ,SUM(column3)
 FROM
-    my_table
+    my_table my
+JOIN
+    some_other_table sot
+ON
+    my.column1=sot.column6
 where
     column1>0
     and column2='ala ma kota'
